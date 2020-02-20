4bdf0b6388a2cf620692d0d9f8c0e32d6aa6d57e
espinozaerica74 <espinozaerica74@gmail.com>
2020-02-20 02:32:37 +0000

    add input add button add fetch

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 
 console.log('hi');
 
-$('h1').click(function(){
+$('button').click(function(){
+  fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+  .then(function(response) {
+        return response.json();
+  })
+  .then(function(data) {
+        // function body
+        console.log(data);
+
+        });
+      
+
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });

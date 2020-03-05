188829636c1e4cd899a0408fc1cf6739a8bb8a97
espinozaerica74 <espinozaerica74@gmail.com>
2020-03-05 02:20:52 +0000

    change backgrounds color add img input add a url var

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@
 console.log('data');
 
 $('button').click(function(){
-  fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+   var input = $("input").val();
+   var url = "https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=" + input;
+  
+  fetch(url)
+
   .then(function(response) {
         return response.json();
   })
   .then(function(data) {
         // function body
+       
         console.log(data.data[0].images.original.url);
         var originalLink= data.data[0].images.original.url;
-        
+        $("body").append(`<img src="${originalLink}">`);
+       
+    
         });
       
 
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+    
 });
